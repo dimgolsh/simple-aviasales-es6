b857f39fa9c9d9e5e189da5e8fe02723258c6996
dimon <dimagol64@gmail.com>
2020-03-19 11:45:16 +0300

    Second

--- script.js
+++ script.js
@@ -7,112 +7,80 @@ let formSearch = document.querySelector(".form-search"),
   dropdownCitiesTo = document.querySelector(".dropdown__cities-to"),
   inputDateDepart = document.querySelector(".input__date-depart");
 
-  //города 
+//города
 
 //let citiesApi = 'http://api.travelpayouts.com/data/ru/cities.json';
-let citiesApi = 'dataBase/cities.json';
-let proxy = 'https://cors-anywhere.herokuapp.com/';
+let citiesApi = "dataBase/cities.json";
+let proxy = "https://cors-anywhere.herokuapp.com/";
 
-
-let city = [
-
-];
-
+let city = [];
 
 let getData = (url, callback) => {
- 
+  let request = new XMLHttpRequest();
 
-    let request  = new XMLHttpRequest();
+  request.open("GET", url);
 
-    request.open('GET', url);
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState !== 4) return;
 
-    request.addEventListener('readystatechange', () => {
+    if (request.status === 200) {
+      callback(request.response);
+    } else {
+      console.error(request.status);
+    }
+  });
 
-     if(request.readyState !== 4) return;
-
-     if(request.status === 200){
-
-        callback(request.response);
-     } else {
-         console.error(request.status);
-     }
-
-    });
-    
-    request.send();
-
+  request.send();
 };
 
-
-
-
-let showCity = function(input, list){
-
-    list.textContent = "";
+let showCity = function(input, list) {
+  list.textContent = "";
 
   if (input.value !== "") {
     let filterCity = city.filter(item => {
-      let fixItem = item.toLocaleLowerCase();
-
-      return fixItem.includes(input.value.toLocaleLowerCase());
+        let fixItem = item.name.toLocaleLowerCase();
+       return fixItem.includes(input.value.toLocaleLowerCase());
     });
 
-    filterCity.forEach(item => {
+    filterCity.forEach((item) => {
+
+   
       let li = document.createElement("li");
       li.classList.add("dropdown__city");
-      li.textContent = item;
+      li.textContent = item.name;
       list.append(li);
     });
   }
-
-
 };
-
 
 let selectCity = (e, input, list) => {
-
-    let target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        input.value = target.textContent;
-        list.textContent = '';
-        }
-
+  let target = event.target;
+  if (target.tagName.toLowerCase() === "li") {
+    input.value = target.textContent;
+    list.textContent = "";
+  }
 };
 
-
-inputCitiesTo.addEventListener('input', () => {
-    showCity(inputCitiesTo, dropdownCitiesTo)
-})
-
-inputCitiesFrom.addEventListener('input', () => {
-
-    showCity(inputCitiesFrom, dropdownCitiesFrom)
+inputCitiesTo.addEventListener("input", () => {
+  showCity(inputCitiesTo, dropdownCitiesTo);
 });
 
-
-dropdownCitiesFrom.addEventListener('click',(event) => {
-    selectCity(event,inputCitiesFrom,dropdownCitiesFrom )
+inputCitiesFrom.addEventListener("input", () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
-dropdownCitiesTo.addEventListener('click',(event)  => {
-    selectCity(event,inputCitiesTo, dropdownCitiesTo)
+dropdownCitiesFrom.addEventListener("click", event => {
+  selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
 
+dropdownCitiesTo.addEventListener("click", event => {
+  selectCity(event, inputCitiesTo, dropdownCitiesTo);
+});
 
-getData(citiesApi, (data)=>{
+getData(citiesApi, data => {
 
-    let dataCities =  JSON.parse(data);
-
-
-   city = dataCities.filter((item) => {
-
-     console.log(item.name);
-     
-      return true;
-    });
-
-
-
-
-
+  city = JSON.parse(data).filter((item)=> {
+    return item.name;
   });
+
+});
